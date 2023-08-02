749af8186b58136d6c2bb0cd1760c0e209341ace
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-08-02 23:29:29 +0600

    up

---
+++ src/Services/User/user.service.ts
@@ -0,0 +1,15 @@
+import { db } from "../../utils/db.server";
+
+export const getUserList = async () => {
+  const users = await db.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  return users;
+};
