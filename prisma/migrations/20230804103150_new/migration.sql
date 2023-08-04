2cc8951b29c25adc390fcdf8d4431f7f9418d5b7
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-08-04 16:45:19 +0600

    update

--- prisma/migrations/20230804103150_new/migration.sql
+++ prisma/migrations/20230804103150_new/migration.sql
@@ -24,7 +24,6 @@ CREATE TABLE "Profile" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "bio" VARCHAR(250),
-    "image" TEXT,
     "address" VARCHAR(250) NOT NULL,
     "district" VARCHAR(250),
     "city" VARCHAR(250) NOT NULL,
@@ -32,8 +31,21 @@ CREATE TABLE "Profile" (
     "zipCode" VARCHAR(80),
     "gender" VARCHAR(80),
     "birthDate" DATE,
+    "profileImageId" INTEGER,
 
     CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProfileImage" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "profileId" INTEGER,
+
+    CONSTRAINT "ProfileImage_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -52,7 +64,22 @@ CREATE UNIQUE INDEX "User_email_phone_userName_key" ON "User"("email", "phone", 
 CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Profile_profileImageId_key" ON "Profile"("profileImageId");
+
+-- CreateIndex
 CREATE INDEX "user_address" ON "Profile"("userId", "address");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProfileImage_userId_key" ON "ProfileImage"("userId");
+
+-- CreateIndex
+CREATE INDEX "user_profile_index" ON "ProfileImage"("userId", "profileId");
 
 -- AddForeignKey
 ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_profileImageId_fkey" FOREIGN KEY ("profileImageId") REFERENCES "ProfileImage"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfileImage" ADD CONSTRAINT "ProfileImage_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
