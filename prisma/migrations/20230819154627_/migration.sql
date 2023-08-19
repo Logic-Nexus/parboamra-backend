e9422f5a7a8fbf2426ce401be91ce2eca4270cdf
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-08-20 01:00:13 +0600

    up

--- prisma/migrations/20230819154627_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `expireAt` to the `RegisterOtp` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "RegisterOtp" ADD COLUMN     "expireAt" TIMESTAMP(6) NOT NULL,
-ADD COLUMN     "usingFor" VARCHAR(250);
